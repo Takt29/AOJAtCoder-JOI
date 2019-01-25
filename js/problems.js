05560f5b70c814545fe732a18c800b9d1f4e262a
Takuto <takuto.yoshida@gmail.com>
2019-01-25 12:59:53 +0900

    add atcoderLink

--- js/problems.js
+++ js/problems.js
@@ -1,5 +1,5 @@
 $(function(){
-  
+
   //難易度別 背景色 - 文字色
   var level_color = {
     "-1": {back:"#B3B3B3", str:"#FFFFFF"},
@@ -35,7 +35,7 @@ $(function(){
       if (a_2 != -1 && b_2 == -1) return reverse * 1;
       if (a_2 > b_2) return reverse * -1;
       if (a_2 < b_2) return reverse * 1;
-      
+
       return 0;
     }
   }
@@ -52,7 +52,7 @@ $(function(){
       callback(solved);
       return;
     }
-    
+
     //AtCoder 状態取得
     if (user.atcoder) {
       await axios({
@@ -73,7 +73,7 @@ $(function(){
         failed_atcoder = true;
       })
     }
-    
+
     //AOJ 状態取得
     if (user.aoj) {
       await axios({
@@ -92,7 +92,7 @@ $(function(){
         failed_aoj = true;
       })
     }
-  
+
     await callback(solved, failed_aoj, failed_atcoder);
   }
 
@@ -100,17 +100,17 @@ $(function(){
   INIT_YEAR_BEGIN = "2007";
   INIT_YEAR_END   = "2019";
   INIT_SORT       = "level";
-  
+
 
   //パラメータ取得
   var arg  = new Object;
   url = location.search.substring(1).split('&');
-  
+
   for(i=0; url[i]; i++) {
     var k = url[i].split('=');
     arg[k[0]] = k[1];
   }
-  
+
   //パラメータ確認
   if(("year_begin" in arg == false) || ("year_end" in arg == false)){
     arg.year_begin = INIT_YEAR_BEGIN;
@@ -120,12 +120,12 @@ $(function(){
     arg.con_sc = "1";
     arg.con_op = "0";
   }
-  
+
   if(("rival_aoj_userid" in arg == false) || ("rival_atcoder_userid" in arg == false)){
     arg.rival_aoj_userid = "";
     arg.rival_atcoder_userid = "";
   }
-  
+
   if("sort" in arg == false){
     arg.sort = INIT_SORT;
     arg.Batch = "1";
@@ -147,10 +147,11 @@ $(function(){
     arg.Batch = "1";
     arg.Communication = "1";
     arg.OutputOnly = "0";
+    arg.beta_url = "1";
   }
 
   var beta_url = arg.beta_url === "1"
-  
+
   $("#con_yo").prop('checked', arg.con_yo === "1");
   $("#con_ho").prop('checked', arg.con_ho === "1");
   $("#con_sc").prop('checked', arg.con_sc === "1");
@@ -171,12 +172,12 @@ $(function(){
                  "aoj" : arg.aoj_userid};
   var rival_id = {"atcoder" : arg.rival_atcoder_userid,
                   "aoj" : arg.rival_aoj_userid};
-  
+
   //統計 初期化
   var user_ac_counter = [];
   var rival_ac_counter = [];
   var problem_counter = [];
-  
+
   for(var i=-1;i<=12;i++){
     user_ac_counter[i] = 0;
     rival_ac_counter[i] = 0;
@@ -198,12 +199,12 @@ $(function(){
     });
 
     $("#error_msg").text("情報取得中...(最大20秒)");
-    
+
     get_user_status(aoj_to_problemid, atcoder_to_problemid, user_id, function(user_ac, failed_aoj_user, failed_atcoder_user){
 
       get_user_status(aoj_to_problemid, atcoder_to_problemid, rival_id, function(rival_ac, failed_aoj_rival, failed_atcoder_rival){
 
-        
+
         $("div.user_color").html('');
 
         if(rival_id.aoj == "" && rival_id.atcoder == ""){
@@ -218,7 +219,7 @@ $(function(){
         $("table.problems tbody").html(
           '<tr><th>難易度</th><th>問題名</th><th>AOJ</th><th>出典</th></tr>'
         );
-        
+
         //並び替え
         if(arg.sort == 'id')
           data.sort(sort_by("problem_id","level","false"));
@@ -234,7 +235,7 @@ $(function(){
           if(arg.con_op !== "1" && "07" <= this.problem_id.substr(2,2) && this.problem_id.substr(2,2) <= "08") return;
 
           if(arg[this.tasktype] != "1") return;
-          
+
           //統計カウント
           if(this.aoj_id !== "" || this.atcoder_id !== ""){
             if(user_ac[this.problem_id] == "1"){
@@ -245,12 +246,12 @@ $(function(){
             }
             problem_counter[this.level]++;
           }
-          
+
           //表示関連
           var level_str = this.level;
-          
+
           if(this.level == -1) level_str = "?";
-          
+
           var td_class = "";
           if(this.aoj_id !== "" || this.atcoder_id !== ""){
             if(user_ac[this.problem_id] == "1"){
@@ -258,9 +259,9 @@ $(function(){
             }else{
               td_class += "none";
             }
-          
+
             td_class += "-";
-            
+
             if(rival_ac[this.problem_id] == "1"){
               td_class += "ac";
             }else{
@@ -279,11 +280,11 @@ $(function(){
           }else{
             atcoder_link = this.name;
           }
-          
+
           var aoj_link = "";
           if(this.aoj_id !== ""){
             var stage = this.source.match(/予選/) ? 'Prelim' : 'Final';
-                
+
             if(!beta_url)
               aoj_link = '<a href="http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id='+this.aoj_id+'"  target="_blank">★</a>';
             else
@@ -304,7 +305,7 @@ $(function(){
               $("#error_msg").text("");
             }
           }
-          
+
           //表示
           $('<tr>'+
             '<td style="background:'+level_color[this.level].back+'; text-align:center; font-weight:bold;">'+
@@ -318,18 +319,18 @@ $(function(){
            ).appendTo('table.problems tbody');
 
         });
-          
+
         //統計表
         var stat_header = '';
-        
+
         var solved_rate = 0;
-        
+
         $("table.statistics tbody").html('');
         for(var i=1;i<=12;i++){
           stat_header += '<th style="background:'+level_color[i].back+'"><font color="'+level_color[i].str+'">'+i+'</font></th>';
         }
         stat_header += '<th style="background:'+level_color["-1"].back+'">?</th><th>ALL</th>';
-        
+
         var add_statistics = function(user, ac_counter, problem_counter, rival){
           //ユーザー名
           if(rival){
@@ -341,56 +342,56 @@ $(function(){
               '<span class="user-mark">'+user.aoj+'/'+user.atcoder+'</span></td></tr>'
              ).appendTo("table.statistics tbody");
           }
-          
+
           //ヘッダー
           $('<tr>'+stat_header+'</tr>').appendTo("table.statistics tbody");
-          
+
           //回答数/割合
           var ac_counter_all = 0;
           var problem_counter_all = 0;
           var stat_body1 = '';
           var stat_body2 = '';
-          
+
           for(var i=1;i<=12;i++){
             if(problem_counter[i] > 0)
               solved_rate = Math.floor(100*ac_counter[i]/problem_counter[i]);
             else
               solved_rate = "--";
-            
+
             stat_body1 += '<td>'+ac_counter[i]+'/'+problem_counter[i]+'</td>';
             stat_body2 += '<td>'+solved_rate+'%</td>';
             ac_counter_all += ac_counter[i];
             problem_counter_all += problem_counter[i];
           }
-          
+
           // ?
           if(problem_counter[-1] > 0)
             solved_rate = Math.floor(100*ac_counter[-1]/problem_counter[-1]);
           else
             solved_rate = "--";
-          
+
           stat_body1 += '<td>'+ac_counter[-1]+'/'+problem_counter[-1]+'</td>';
           stat_body2 += '<td>'+solved_rate+'%</td>';
           ac_counter_all += ac_counter[-1];
           problem_counter_all += problem_counter[-1];
-          
+
           // ALL
           if(problem_counter_all > 0)
             solved_rate = Math.floor(100*ac_counter_all/problem_counter_all);
           else
             solved_rate = "--";
-          
+
           stat_body1 += '<td>'+ac_counter_all+'/'+problem_counter_all+'</td>';
           stat_body2 += '<td>'+solved_rate+'%</td>';
-          
+
           $('<tr>'+stat_body1+'</tr><tr>'+stat_body2+'</tr>').appendTo("table.statistics tbody");
         }
-        
+
         add_statistics(user_id, user_ac_counter, problem_counter,false);
-        
+
         if((rival_id.aoj == "" && rival_id.atcoder == "") == false)
           add_statistics(rival_id, rival_ac_counter, problem_counter,true);
-      });  
+      });
     });
   });
 
