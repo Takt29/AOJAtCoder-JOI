e9f14ed721f76bb2f0bbd7093a97be73f3405fa6
Yoshida Takuto <takuto.yoshida@gmail.com>
2019-09-13 19:11:39 +0900

    BrowserRouterに変更

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { HashRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import { HomeView, HistoryView, LinkView } from './view'
 import { TopBar } from './components'
 
 render(
   <div>
-    <HashRouter>
+    <BrowserRouter>
       <div>
-        <TopBar isHashRouter={true} />
+        <TopBar isHashRouter={false} />
         <Container>
           <Route exact={true} path="/" component={HomeView} />
           <Route exact={true} path="/history" component={HistoryView} />
           <Route exact={true} path="/link" component={LinkView} />
         </Container>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   </div>,
   document.getElementById('app')
 );
