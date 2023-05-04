f48e9801bf6f9d17abde08cb9aa7fdbf1bf0ba1a
Yoshida Takuto <takuto.yoshida@gmail.com>
2023-05-04 12:50:59 +0900

    Firebase Storageにdataファイルを移動

--- frontend/src/api/contest.ts
+++ frontend/src/api/contest.ts
@@ -2,6 +2,8 @@ import axios, { AxiosResponse } from 'axios'
 import { Contest } from '../types/contest'
 
 export const fetchContests = async (): Promise<Contest[]> => {
-  const res: AxiosResponse<Contest[]> = await axios.get('/data/contests.json')
+  const res: AxiosResponse<Contest[]> = await axios.get(
+    'https://storage.googleapis.com/aojatcoder-joi.appspot.com/data/contests.json',
+  )
   return res.data
 }
