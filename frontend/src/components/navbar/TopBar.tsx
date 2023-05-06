37463ad55f89ce99feca835143c85cb2ea62b2b7
Yoshida Takuto <takuto.yoshida@gmail.com>
2023-05-06 18:12:16 +0900

    TopBarのスタイルを整えた

--- frontend/src/components/navbar/TopBar.tsx
+++ frontend/src/components/navbar/TopBar.tsx
@@ -1,36 +1,75 @@
-import { Link as ReactRouterLink } from 'react-router-dom'
-import { Box, Flex, HStack, useColorModeValue } from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  useBreakpointValue,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
 import { TopBarLink } from './TopBarLink'
+import { InternalLink } from '../common/InternalLink'
 
 export const TopBar = () => {
+  const isMobile = useBreakpointValue({ base: true, md: false })
+  const { isOpen, onToggle } = useDisclosure()
+
   return (
     <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} paddingX={8}>
-        <Flex h={16} justifyContent={'space-between'}>
-          <Flex alignItems={'center'}>
-            <HStack>
-              <Box
-                as={ReactRouterLink}
-                to='/'
-                _hover={{ textDecoration: 'none' }}
-              >
-                AOJ/AtCoder-JOI
-              </Box>
-              <HStack
-                as={'nav'}
-                spacing={4}
-                display={{ base: 'none', md: 'flex' }}
-              >
-                <TopBarLink to={'/'} title='List' />
-                <TopBarLink to={'/history'} title='History' />
-                <TopBarLink to={'/changelog'} title='ChangeLog' />
-                <TopBarLink to={'/links'} title='Links' />
-              </HStack>
+      <Box bg={'gray.100'}>
+        <Flex
+          height={16}
+          align={'center'}
+          borderBottom={1}
+          borderStyle={'solid'}
+          borderColor={'gray.200'}
+          paddingX={4}
+        >
+          <Flex
+            flex={{ base: 1 }}
+            display={isMobile ? 'flex' : 'none'}
+            ml={{ base: -2 }}
+          >
+            <IconButton
+              onClick={onToggle}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={isOpen ? 'open navigation' : 'close navigation'}
+              variant={'ghost'}
+            />
+          </Flex>
+          <Flex
+            flex={{ base: 2, md: 'none' }}
+            justifyContent={{ base: 'center', md: 'left' }}
+          >
+            <InternalLink
+              to='/'
+              _hover={{ textDecoration: 'none' }}
+              textAlign={{ base: 'center', md: 'left' }}
+              verticalAlign={'center'}
+              whiteSpace={'nowrap'}
+            >
+              AOJ/AtCoder-JOI
+            </InternalLink>
+          </Flex>
+          <Flex
+            flex={{ base: 'none', md: 'auto' }}
+            display={isMobile ? 'none' : 'flex'}
+          >
+            <HStack as={'nav'} spacing={4} marginLeft={8}>
+              <TopBarLink to={'/'} title='List' />
+              <TopBarLink to={'/history'} title='History' />
+              <TopBarLink to={'/changelog'} title='ChangeLog' />
+              <TopBarLink to={'/links'} title='Links' />
             </HStack>
           </Flex>
-          <Flex alignItems={'center'}>
-            <Box>🇯🇵 / 🇺🇸</Box>
-          </Flex>
+          <Box
+            flex={{ base: 1, md: 0 }}
+            justifyContent={'flex-end'}
+            textAlign={'right'}
+            whiteSpace={'nowrap'}
+          >
+            🇯🇵 / 🇺🇸
+          </Box>
         </Flex>
       </Box>
     </>
